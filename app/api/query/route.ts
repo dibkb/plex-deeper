0ca0540b80da9a88e9e9073d1a31ff1e6b129ae2
Dibas K Borborah <dibas9110@gmail.com>
2025-09-01 11:49:41 +0530

    chore: type safety

--- app/api/query/route.ts
+++ app/api/query/route.ts
@@ -2,37 +2,52 @@ import { taskQueue } from "@/lib/queues";
 import { db } from "@/src/db";
 import { queryResultsTable } from "@/src/schema";
 import { googleSearch } from "@/src/tools/google-search";
+import { ErrorResponse } from "@/src/types/http-response";
 import { Status } from "@/src/types/status";
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const runtime = "nodejs";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const query = searchParams.get("search");
-  if (!query) {
-    return NextResponse.json({ error: "Query is required" }, { status: 400 });
+export interface QueryResponse {
+  jobId: string;
+  insertedId: string;
+}
+
+export async function GET(
+  request: Request
+): Promise<NextResponse<QueryResponse | ErrorResponse>> {
+  try {
+    const { searchParams } = new URL(request.url);
+    const query = searchParams.get("search");
+    if (!query) {
+      return NextResponse.json({ error: "Query is required" }, { status: 400 });
+    }
+    const googleSearchResponse = await googleSearch(query);
+
+    const [insertedRow] = await db
+      .insert(queryResultsTable)
+      .values({
+        id: crypto.randomUUID().toString(),
+        query,
+        searchResults: googleSearchResponse.data,
+        status: Status.PENDING,
+      })
+      .returning({ insertedId: queryResultsTable.id })
+      .execute();
+
+    const job = await taskQueue
+      .createJob({ jobId: insertedRow.insertedId.toString() })
+      .save();
+    const response = NextResponse.json({
+      jobId: job.id,
+      insertedId: insertedRow.insertedId,
+    });
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
   }
-  const response = await googleSearch(query);
-
-  const [insertedRow] = await db
-    .insert(queryResultsTable)
-    .values({
-      id: crypto.randomUUID().toString(),
-      query,
-      searchResults: response.data,
-      status: Status.PENDING,
-    })
-    .returning({ insertedId: queryResultsTable.id })
-    .execute();
-
-  const job = await taskQueue
-    .createJob({ jobId: insertedRow.insertedId.toString() })
-    .save();
-
-  return NextResponse.json({
-    jobId: job.id,
-    insertedId: insertedRow.insertedId,
-  });
 }
