23427fa370524554cdc0acc04e1ffbca3eb682ba
Dibas K Borborah <dibas9110@gmail.com>
2025-09-01 11:55:55 +0530

    fix: remove any

--- lib/queue.ts
+++ lib/queue.ts
@@ -1,6 +1,6 @@
 import Queue from "bee-queue";
 
-export function createQueue<T = any>(name: string, isWorker = false) {
+export function createQueue<T>(name: string, isWorker = false) {
   return new Queue<T>(name, {
     redis: { host: "127.0.0.1", port: 9000 },
     isWorker,
