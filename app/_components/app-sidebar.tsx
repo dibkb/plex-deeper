299e76d9e6032ffde6d3f7ec42e4e59655482007
Dibas K Borborah <dibas9110@gmail.com>
2025-09-03 04:57:34 +0530

    feat: history render

--- app/_components/app-sidebar.tsx
+++ app/_components/app-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Sidebar,
   SidebarContent,
@@ -6,13 +7,42 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
 } from "@/components/ui/sidebar";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { HistoryResponse } from "../api/history/route";
+import { useParams, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export function AppSidebar() {
+  const { data, isLoading, error } = useQuery<HistoryResponse[]>({
+    queryKey: ["history"],
+    queryFn: () =>
+      axios<HistoryResponse[]>("/api/history").then((res) => res.data),
+    enabled: true,
+    refetchInterval: 1000,
+  });
+  const router = useRouter();
+  const { qid } = useParams();
+  const historyRender = data?.map((history) => (
+    <button
+      key={history.id}
+      className={cn(
+        "cursor-pointer w-full text-left truncate text-sm text-zinc-500 dark:text-zinc-500 px-2 py-1 font-medium",
+        qid === history.id && "text-zinc-900 dark:text-zinc-100"
+      )}
+      onClick={() => {
+        router.push(`/query/${history.id}`);
+      }}
+    >
+      {history.query}
+    </button>
+  ));
   return (
     <Sidebar className="bg-background">
       <SidebarHeader />
       <SidebarContent>
         <SidebarGroupLabel>Search History</SidebarGroupLabel>
+        <SidebarGroup className="">{historyRender}</SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
