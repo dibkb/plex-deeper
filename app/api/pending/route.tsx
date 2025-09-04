2f30412055e1c4f58908b1d0bd74b6d42c2272f4
Dibas K Borborah <dibas9110@gmail.com>
2025-09-04 21:18:24 +0530

    feat: peding endpoint

---
+++ app/api/pending/route.tsx
@@ -0,0 +1,39 @@
+import { db } from "@/src/db";
+import { QueryResult, queryResultsTable } from "@/src/schema";
+import { GoogleSearchResults } from "@/src/types/google-search-results";
+import { ErrorResponse } from "@/src/types/http-response";
+import { Status } from "@/src/types/status";
+import { asc, eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const runtime = "nodejs";
+export interface PendingResponse {
+  id: string;
+  searchResults: GoogleSearchResults[];
+}
+export async function GET(
+  request: Request
+): Promise<NextResponse<PendingResponse[] | ErrorResponse>> {
+  try {
+    const results = await db
+      .select({
+        id: queryResultsTable.id,
+        searchResults: queryResultsTable.searchResults,
+      })
+      .from(queryResultsTable)
+      .where(eq(queryResultsTable.status, Status.PENDING_WEB_SCRAPING))
+      .orderBy(asc(queryResultsTable.createdAt))
+      .limit(3)
+      .execute();
+    return NextResponse.json(results);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error instanceof Error ? error.message : "Something went wrong",
+      },
+      { status: 500 }
+    );
+  }
+}
