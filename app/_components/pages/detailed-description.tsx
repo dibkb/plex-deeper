0021b51a62818467ac859db1c7962557119d0c1e
Dibas K Borborah <dibas9110@gmail.com>
2025-09-06 12:36:47 +0530

    feat: laodng screan

--- app/_components/pages/detailed-description.tsx
+++ app/_components/pages/detailed-description.tsx
@@ -20,66 +20,45 @@ export function DetailedDescription({
 }: DetailedDescriptionProps) {
   switch (status) {
     case Status.PENDING:
-      return (
-        <LoadingScreen
-          title="Pending"
-          subtitle="We are processing your query"
-        />
-      );
+      return <Timeline activeStep={Status.PENDING} />;
     case Status.PENDING_WEB_SCRAPING:
-      return (
-        <LoadingScreen
-          title="Web scraping in progress"
-          subtitle="We are gathering all the information from the web"
-        />
-      );
+      return <Timeline activeStep={Status.PENDING_WEB_SCRAPING} />;
     case Status.WEB_SCRAPING_COMPLETED:
-      return (
-        <LoadingScreen
-          title="Web scraping completed"
-          subtitle="We gathered all the information we need to generate a detailed response"
-        />
-      );
+      return <Timeline activeStep={Status.WEB_SCRAPING_COMPLETED} />;
     case Status.GENERATING_RESPONSE:
-      return (
-        <LoadingScreen
-          title="Generating response"
-          subtitle="We are generating a detailed response to your query"
-        />
-      );
+      return <Timeline activeStep={Status.GENERATING_RESPONSE} />;
     case Status.SUCCESS:
       return (
-        <Timeline activeStep={Status.GENERATING_RESPONSE} />
-        // <main className="my-2">
-        //   <ImagesPreview images={images || []} />
-        //   <div className="flex flex-col gap-2 pb-4">
-        //     {detailedDescription?.map((item) => (
-        //       <div key={item.heading}>
-        //         <h3 className={cn("text-lg font-medium mb-2")}>
-        //           {item.heading}
-        //         </h3>
-        //         <p
-        //           className={cn(" text-zinc-800 dark:text-zinc-200 text-base")}
-        //         >
-        //           {item.paragraphs?.join(" ")}
-        //         </p>
-        //         <ul className="grid gap-2">
-        //           {item.bulletPoints?.map((point) => (
-        //             <li
-        //               key={point}
-        //               className={cn(
-        //                 "text-zinc-800 dark:text-zinc-200 text-base grid grid-cols-[auto_1fr] gap-2 items-start"
-        //               )}
-        //             >
-        //               <DotIcon className="w-4 h-4 text-zinc-800 dark:text-zinc-200 mt-1 flex-shrink-0" />
-        //               <span>{point}</span>
-        //             </li>
-        //           ))}
-        //         </ul>
-        //       </div>
-        //     ))}{" "}
-        //   </div>
-        // </main>
+        <main className="my-2">
+          <ImagesPreview images={images || []} />
+          <div className="flex flex-col gap-2 pb-4">
+            {detailedDescription?.map((item) => (
+              <div key={item.heading}>
+                <h3 className={cn("text-lg font-medium mb-2")}>
+                  {item.heading}
+                </h3>
+                <p
+                  className={cn(" text-zinc-800 dark:text-zinc-200 text-base")}
+                >
+                  {item.paragraphs?.join(" ")}
+                </p>
+                <ul className="grid gap-2">
+                  {item.bulletPoints?.map((point) => (
+                    <li
+                      key={point}
+                      className={cn(
+                        "text-zinc-800 dark:text-zinc-200 text-base grid grid-cols-[auto_1fr] gap-2 items-start"
+                      )}
+                    >
+                      <DotIcon className="w-4 h-4 text-zinc-800 dark:text-zinc-200 mt-1 flex-shrink-0" />
+                      <span>{point}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}{" "}
+          </div>
+        </main>
       );
     case Status.ERROR:
       return <div>Error</div>;
